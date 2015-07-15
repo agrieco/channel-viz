f06a4809fc36e19e829e031788541e49c1944b0c
agrieco <agrieco@users.noreply.github.com>
2015-07-15 19:31:47 -0400

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,8 +12,8 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
+	var defaultKey		= 'M1IEQHJopqm28ARt6BTZXhn65ue3JPsfqHh9loB4beKROiT6', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [2089913180], // Comma separated array of Xively Feed ID numbers
 		applicationName	= '', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
